7fa9f55dbd9436bbcd3236b3be06aa47bcdf5cdd
HuynhLQ <huynhlq.dev@gmail.com>
2024-07-31 19:56:37 +0700

    Initial Commit

---
+++ EventCountdown/EventCountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventCountdownApp.swift
+//  EventCountdown
+//
+//  Created by huynh on 31/7/24.
+//
+
+import SwiftUI
+
+@main
+struct EventCountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
